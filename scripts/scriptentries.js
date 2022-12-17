af443d9e290b51f8ca1275ca5bd847b6aedbf5a6
John Stanley Altonaga <74578077+twapegg@users.noreply.github.com>
2022-12-18 05:59:33 +0800

    Delete scripts directory

--- scripts/scriptentries.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-  const popup = document.querySelector(".popup");
-  const rect = popup.getBoundingClientRect();
-  if (rect.top < window.innerHeight) {
-    popup.classList.add("in-view");
-  }
-});
